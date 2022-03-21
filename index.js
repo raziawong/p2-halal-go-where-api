e9c1f59270a10551e66a6bba366e0f05a9976d4c
Razia <nrwpc.stack@gmail.com>
2022-03-21 11:32:54 +0800

    Update routes for Countries collection
    - refactor codes
    - Update GET
    - Add POST, PATCH

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ const DB_REL = {
     articles: 'articles'
 };
 
+const REGEX = {
+    location_name: /[A-Za-zÀ-ȕ\s\-]/
+}
+
 async function main() {
     await connect(process.env.MONGO_URI, DB_REL.name);
 
@@ -28,7 +32,7 @@ async function main() {
     function sendInvalidError(res, details) {
         res.status(406);
         res.json({ 
-            main: "Not Acceptable. Request has failed validation.",
+            main: 'Not Acceptable. Request has failed validation.',
             details
         });
     }
@@ -41,15 +45,162 @@ async function main() {
         });
     }
 
+    async function getCountries(code, name, city, showCity=false) {
+        let criteria = {};
+        let projection = { 
+            'projection': {
+                'code': 1,
+                'name': 1,
+            }
+        };
+
+        if (showCity) {
+            projection.projection.cities = 1;
+        }
+
+        if (code) {
+            criteria.code = {
+                '$regex': code,
+                '$options': 'i',
+            };
+        }
+        if (name) {
+            criteria.name = {
+                '$regex': name,
+                '$options': 'i',
+            };
+        }
+        if (city) {
+            criteria.cities = {
+                '$elemMatch': {
+                    'name': {
+                        '$regex': city,
+                        '$options': 'i',
+                    }
+                }
+            }
+        }
+
+        let countries = await getDB()
+            .collection(DB_REL.countries)
+            .find(criteria, projection).toArray();
+        return countries;
+    }
+
+    async function validateCountries({ code, name, cities }, isNew=true) {
+        let validation = [];
+        let countries = await getCountries(code, null);
+
+        if (!code) {
+            validation.push({'field': 'code', 'error': 'Country Code is required'});
+        } else if (code.length > 2) {
+            validation.push({'field': 'code', 'value': code, 'error': 'Country Code must use ISO 3166-1 alpha-2'});
+        } else if (isNew && countries.length) {
+            validation.push({'field': 'code', 'error': 'Country Code already exists, please do update instead'});
+        } else if (!isNew && !countries.length) {
+            validation.push({'field': 'code', 'value': code, 'error': 'Country Code does not exists, please do create instead'});
+        }
+
+        if (!name) {
+            validation.push({'field': 'name', 'error': 'Country Name is required'});
+        } else if (!REGEX.location_name.match(name)) {
+            validation.push({'field': 'name', 'value': name, 'error': 'Country Name cannot contain special characters'});
+        }
+
+        if (!cities) {
+            validation.push({'field': 'cities', 'error': 'Country needs to have at least one city'});
+        } else {
+            cities = cities.map(c => {
+                if (!REGEX.location_name.match(c.name)) {
+                    validation.push({'field': 'cities', 'value': c.name, 'error': 'City name cannot contain special characters'});
+                }
+                return c;
+            });
+        }
+
+        if (code && cities) {
+            cities = cities.map(c => {
+                let country = await getCountries(code, undefined, c.name);
+                if (country) {
+                    validation.push({'field': 'cities', 'value': c.name, 'error': 'City name already exists in Country ' + code});
+                }
+                return c;
+            });
+        }
+
+        return validation;
+    }
+
     app.get('/countries', async(req, res) => {
         try {
-            let criteria = {};
-            if (req.query.code) {
-                criteria.code = {
-                    '$regex': req.query.code,
-                    '$options': 'i',
-                };
+            let { code, name } = req.query;
+            let countries = await getCountries(code, name);
+            sendSuccess(res, countries);
+        } catch (err) {
+            sendServerError(res, 'Error encountered while reading countries collection.');
+        }
+    });
+
+    app.post('/countries', async(req, res) => {
+        try {
+            let { code, name, cities } = req.body;
+            let validation = await validateCountries(req.body, true);
+            
+            if (!validation.length){
+                code = code.toUpperCase();
+                cities = cities.map(c => {
+                    c._id = new ObjectId();
+                    return c;
+                })
+                let country = await getDB.collection(DB_REL.countries).insertOne({code, name, cities});
+                sendSuccess(res, country);
+            } else {
+                sendInvalidError(res, validation);
             }
+        } catch (err) {
+            sendServerError(res, 'Error encountered while adding to countries collection.');
+        }
+    });
+
+    app.patch('/countries', async(req, res) => {
+        try {
+            let { code, name, cities } = req.body;
+            let validation = await validateCountries(req.body, false);
+            
+            if (!validation.length){
+                let update = {
+                    '$set': {}
+                };
+                if (name) {
+                    update.$set.name = name;
+                }
+                if (cities) {
+                    cities = cities.map(c => {
+                        if (!c._id) {
+                            c._id = new ObjectId();
+                        }
+                        return c;
+                    });
+                    update.$set.cities = cities;
+                }
+
+                let country = await getDB
+                    .collection(DB_REL.countries)
+                    .updateOne({ 'code': code }, update);
+                sendSuccess(res, country);
+            } else {
+                sendInvalidError(res, validation);
+            }
+        } catch (err) {
+            sendServerError(res, 'Error encountered while adding to countries collection.');
+        }
+    });
+
+
+
+    app.get('/categories', async(req, res) => {
+        try {
+            let criteria = {};
             if (req.query.name) {
                 criteria.name = {
                     '$regex': req.query.name,
